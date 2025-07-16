612e7718756346c2de69cd291b00d6c4d4015958
mateus andrade <mateusandrade092@gmail.com>
2025-07-15 22:12:50 -0300

    rota de direcionamento pos login acertada

--- js/script_interaction.js
+++ js/script_interaction.js
@@ -136,7 +136,7 @@ async function login_autorization(){
     const password_user = document.getElementById("password")
 
     try{
-        const response_system = await fetch("https://stockbridges.onrender.com/homepage",{
+        const response_system = await fetch("https://stockbridges.onrender.com/login",{
              method: "POST",
              headers:{"Content-Type": "application/json"},
              body: JSON.stringify({   
